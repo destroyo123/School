f540c9c7018dd7fb6cd10df64edec3cfabfbfab0
marenkalberer <marenkalberer@gmail.com>
2026-03-24 13:23:46 -0700

    Update TaylorMaccoll.m

--- Spring2026/AME323/Project1/TaylorMaccoll.m
+++ Spring2026/AME323/Project1/TaylorMaccoll.m
@@ -35,10 +35,12 @@ T_amb = 288.15; % Std sea level temeprature is 273.15 Kelvin?
 M_inputs = [1.5, 2.0, 5.0];
 
 %% Setting Up the System of Equations
-% Sets up the functions of θ, β, M, other crap (is this actually what we wanna do?)
+% Setmks up the functions of θ, β, M, other crap (is this actually what we wanna do?)
 
 %% Solving the System of Equations
 % Solves the system and logs the results.
+
+y1 = v/v_max;
 
 %% Plotting
 % This is where the required plots are generated.
