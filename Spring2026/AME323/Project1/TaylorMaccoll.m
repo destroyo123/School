636db8bd06538547dbf73c955702203e71293762
marenkalberer <marenkalberer@gmail.com>
2026-03-26 09:03:26 -0700

    hi

--- Spring2026/AME323/Project1/TaylorMaccoll.m
+++ Spring2026/AME323/Project1/TaylorMaccoll.m
@@ -198,6 +198,10 @@ vt = v_after * sin(beta_max-tcone_max);                % Max theta component
 y0 = [vr, vt];
 
 
+%% ODE45 TIME %%
+
+[theta_out, y0] = ode45()
+
 %% TESTING %%
 Results = zeros(1,3);
 
